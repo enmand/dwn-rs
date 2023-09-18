f2dae3a8920fbb927f0af8778732f42c99d0adfb
Dan Enman <enmand@gmail.com>
2023-09-18 19:05:43 -0300

    feat: use wee_alloc for wasm build

--- src/js/mod.rs
+++ src/js/mod.rs
@@ -14,6 +14,15 @@ use web_sys::AbortSignal;
 
 extern crate console_error_panic_hook;
 
+use cfg_if::cfg_if;
+
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+
 const INDEX_MAP: &'static str = r#"import { MessageStoreOptions } from "@tbd54566975/dwn-sdk-js";"#;
 
 #[wasm_bindgen]
