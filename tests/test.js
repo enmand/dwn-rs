566a164d310d827bccb29d7a7c903e9b3c0bf688
Dan Enman <enmand@gmail.com>
2024-01-28 02:07:44 -0400

    fix: correct build paths on tests for wasm-pack build

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,14 @@
-import { TestSuite } from '@tbd54566975/dwn-sdk-js/tests';
-import { SurrealDB } from "../out/index.js";
-import WebSocket from 'isomorphic-ws';
+import { TestSuite } from "@tbd54566975/dwn-sdk-js/tests";
+import { SurrealDB } from "../pkg/index.js";
+import WebSocket from "isomorphic-ws";
 
 global.WebSocket = WebSocket;
 
 let s = new SurrealDB();
 // await s.connect("ws://192.168.10.56:8000/");
 await s.connect("mem://");
-describe('Store dependent tests', () => {
-    TestSuite.runStoreDependentTests({
-        messageStore: s,
-    })
-})
+describe("Store dependent tests", () => {
+  TestSuite.runStoreDependentTests({
+    messageStore: s,
+  });
+});
