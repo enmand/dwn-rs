235dd583fffb5fc1c9610bc3ab4b9179e7e13df5
Dan Enman <daniel.enman@snyk.io>
2024-01-31 14:32:32 -0400

    tests: test for datetime serialize

--- crates/dwn-rs-core/src/serde.rs
+++ crates/dwn-rs-core/src/serde.rs
@@ -14,3 +14,33 @@ where
         None => serializer.serialize_none(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::SecondsFormat;
+
+    use crate::Descriptor;
+
+    use super::*;
+
+    #[test]
+    fn test_serialize_optional_datetime() {
+        #[derive(serde::Serialize, serde::Deserialize, Debug)]
+        struct Out {
+            #[serde(rename = "messageTimestamp")]
+            timestamp: String,
+        }
+        let now = Utc::now();
+        let d = Descriptor {
+            timestamp: Some(now.clone()),
+            ..Default::default()
+        };
+
+        let serialized = serde_json::to_string(&d).unwrap();
+        let deserialized: Out = serde_json::from_str(&serialized).unwrap();
+        assert_eq!(
+            deserialized.timestamp,
+            now.to_rfc3339_opts(SecondsFormat::Micros, true)
+        );
+    }
+}
