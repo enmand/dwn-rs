0dc95ee7cdcc0ce48e14d13cfaf91ab1cdb18c0d
Dan Enman <enmand@gmail.com>
2024-02-26 10:04:45 -0400

    test: include data-store tests from dwn-sdk-js

--- crates/dwn-rs-wasm/tests/test.js
+++ crates/dwn-rs-wasm/tests/test.js
@@ -1,5 +1,5 @@
 import { TestSuite } from "@tbd54566975/dwn-sdk-js/tests";
-import { SurrealMessageStore } from "../pkg/index.js";
+import { SurrealDataStore, SurrealMessageStore } from "../pkg/index.js";
 import WebSocket from "isomorphic-ws";
 
 global.WebSocket = WebSocket;
@@ -7,8 +7,11 @@ global.WebSocket = WebSocket;
 let s = new SurrealMessageStore();
 // await s.connect("ws://192.168.10.56:8000/");
 await s.connect("mem://");
+let d = new SurrealDataStore();
+await d.connect("mem://");
 describe("Store dependent tests", () => {
   TestSuite.runStoreDependentTests({
     messageStore: s,
+    dataStore: d,
   });
 });
