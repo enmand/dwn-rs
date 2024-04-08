55f4b5e38ac68ef9f57d028f0e1abd28fc87a52f
Dan Enman <enmand@gmail.com>
2024-04-08 15:54:56 -0300

    feat: add browser tests using Web Test Runner
    Add browser tests to test the WASM integration with dwn-sdk-js to ensure
    the code is portable to Node and Browser environments

---
+++ crates/dwn-rs-wasm/web-test-runner.config.mjs
@@ -0,0 +1,54 @@
+import { rollupBundlePlugin } from "@web/dev-server-rollup";
+
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import alias from "@rollup/plugin-alias";
+import inject from "@rollup/plugin-inject";
+import replace from "@rollup/plugin-replace";
+import wasm from "@rollup/plugin-wasm";
+import { playwrightLauncher } from "@web/test-runner-playwright";
+
+import stdLibBrowser from "node-stdlib-browser";
+
+export default {
+  rootDir: "./",
+  files: "./tests/test.browser.js",
+  coverage: true,
+  browsers: [
+    playwrightLauncher({ product: "chromium" }),
+    playwrightLauncher({ product: "firefox" }),
+    playwrightLauncher({ product: "webkit" }),
+  ],
+  nodeResolve: true,
+  testFramework: "mocha",
+  plugins: [
+    rollupBundlePlugin({
+      rollupConfig: {
+        input: ["tests/test.browser.js"],
+        plugins: [
+          replace({
+            include: ["tests/*.js"],
+            __environment__: '"development"',
+          }),
+          alias({
+            entries: stdLibBrowser,
+          }),
+          resolve({
+            browser: true,
+          }),
+          commonjs(),
+          wasm({
+            include: ["browsers/*.wasm"],
+            maxFileSize: 25600000,
+          }),
+          json(),
+          inject({
+            process: stdLibBrowser.process,
+            Buffer: [stdLibBrowser.buffer, "Buffer"],
+          }),
+        ],
+      },
+    }),
+  ],
+};
