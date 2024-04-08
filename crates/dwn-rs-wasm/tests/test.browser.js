491f3b99ef6b9fbe75740d992b6295416e540c8c
Dan Enman <enmand@gmail.com>
2024-04-08 15:54:56 -0300

    feat: add browser tests using Web Test Runner
    Add browser tests to test the WASM integration with dwn-sdk-js to ensure
    the code is portable to Node and Browser environments

---
+++ crates/dwn-rs-wasm/tests/test.browser.js
@@ -0,0 +1,25 @@
+import { TestSuite } from "@tbd54566975/dwn-sdk-js/tests";
+import init, {
+  SurrealDataStore,
+  SurrealMessageStore,
+  SurrealEventLog,
+} from "../browsers/index.js";
+import stores from "../browsers/index_bg.wasm";
+
+let instance = await stores();
+await init(instance);
+
+let s = new SurrealMessageStore();
+// await s.connect("ws://192.168.10.56:8000/");
+await s.connect("mem://");
+let d = new SurrealDataStore();
+await d.connect("mem://");
+let e = new SurrealEventLog();
+await e.connect("mem://");
+describe("Store dependent tests", () => {
+  TestSuite.runStoreDependentTests({
+    messageStore: s,
+    dataStore: d,
+    eventLog: e,
+  });
+});
