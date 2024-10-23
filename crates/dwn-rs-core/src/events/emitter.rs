f8d11adb4d30066f1e53fdd6b619f841f31180dd
Dan Enman <enmand@gmail.com>
2024-10-22 21:30:05 -0300

    feat: use `Message<D: MessageDescriptor>` as generic Message type

--- crates/dwn-rs-core/src/events/emitter.rs
+++ crates/dwn-rs-core/src/events/emitter.rs
@@ -1,22 +1,38 @@
+use std::fmt::Debug;
+
+use serde::{de::DeserializeOwned, Serialize};
 use xtra::{Address, Mailbox};
 
 use crate::{
+    descriptors::MessageDescriptor,
     errors::{EventStreamError, StoreError},
-    MapValue,
+    MapValue, Message,
 };
 use tracing::{instrument, trace};
 
 use super::{Emit, EventChannel, EventStream, MessageEvent, Shutdown, Subscribe, Subscription};
 
 #[derive(Debug, Default)]
-pub struct EventStreamer(Option<Address<EventStream>>);
-impl EventStreamer {
+pub struct EventStreamer<D>(Option<Address<EventStream<D>>>)
+where
+    Message<D>: Serialize + DeserializeOwned,
+    D: MessageDescriptor + Clone + Debug + PartialEq + Send + 'static;
+
+impl<D> EventStreamer<D>
+where
+    Message<D>: Serialize + DeserializeOwned,
+    D: MessageDescriptor + Clone + Debug + PartialEq + Send + 'static,
+{
+    pub fn new() -> Self {
+        Self(None)
+    }
+
     #[cfg(target_arch = "wasm32")]
     #[instrument]
     pub async fn open(&mut self) {
         trace!("opening EventStreamer (wasm)");
         self.0 = Some(xtra::spawn_wasm_bindgen(
-            EventStream::default(),
+            EventStream::new(),
             Mailbox::unbounded(),
         ));
     }
@@ -25,10 +41,7 @@ impl EventStreamer {
     #[instrument]
     pub async fn open(&mut self) {
         trace!("opening EventStreamer (tokio)");
-        self.0 = Some(xtra::spawn_tokio(
-            EventStream::default(),
-            Mailbox::unbounded(),
-        ));
+        self.0 = Some(xtra::spawn_tokio(EventStream::new(), Mailbox::unbounded()));
     }
 
     #[instrument]
@@ -39,7 +52,7 @@ impl EventStreamer {
     }
 
     #[instrument]
-    pub async fn emit(&self, ns: &str, evt: MessageEvent, indexes: MapValue) {
+    pub async fn emit(&self, ns: &str, evt: MessageEvent<D>, indexes: MapValue) {
         if let Some(addr) = &self.0 {
             let _ = addr
                 .send(Emit {
@@ -56,7 +69,7 @@ impl EventStreamer {
         &self,
         ns: &str,
         id: &str,
-        listener: EventChannel,
+        listener: EventChannel<D>,
     ) -> Result<Subscription, EventStreamError> {
         if let Some(addr) = &self.0 {
             trace!("subscribing to event stream");
---
+++ crates/dwn-rs-stores/src/cid.rs
@@ -0,0 +1,85 @@
+use std::collections::TryReserveError;
+
+use cid::Cid;
+use multihash_codetable::Code;
+use multihash_codetable::MultihashDigest;
+use serde_ipld_dagcbor::EncodeError;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncSeek};
+
+pub fn generate_cid<B>(data: B) -> Result<Cid, EncodeError<TryReserveError>>
+where
+    B: AsRef<[u8]>,
+{
+    let mh = Code::Sha2_256.digest(data.as_ref());
+    let cid = Cid::new_v1(multicodec::Codec::DagCbor.code(), mh);
+
+    Ok(cid)
+}
+
+pub async fn generate_cid_from_asyncreader<R>(
+    reader: R,
+) -> Result<Cid, EncodeError<TryReserveError>>
+where
+    R: AsyncRead + AsyncSeek + Unpin,
+{
+    let mut buf = Vec::new();
+    reader
+        .take(1024 * 1024)
+        .read_to_end(&mut buf)
+        .await
+        .map_err(EncodeError::Write)
+        .unwrap();
+
+    let mh = Code::Sha2_256.digest(&buf);
+    let cid = Cid::new_v1(multicodec::Codec::DagCbor.code(), mh);
+
+    Ok(cid)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+    use std::io::Cursor;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_generate_cid() {
+        let data = json!({
+            "hello": "world",
+        });
+
+        let cid = generate_cid(data.to_string()).unwrap();
+
+        assert_eq!(
+            cid,
+            Cid::from_str("bafyreietui4xdkiu4xvmx4fi2jivjtndbhb4drzpxomrjvd4mdz4w2avra").unwrap(),
+        );
+        assert_eq!(cid.codec(), multicodec::Codec::DagCbor.code());
+    }
+
+    #[tokio::test]
+    async fn test_generate_cid_from_asyncreader() {
+        // Define some sample data to read
+        let data = b"Sample data to generate CID";
+
+        // Create a cursor over the data, which implements AsyncRead + AsyncSeek
+        let cursor = Cursor::new(data);
+
+        // Call the function with the cursor
+        let cid = generate_cid_from_asyncreader(cursor).await;
+        assert!(cid.is_ok());
+        let cid = cid.unwrap();
+
+        // Verify that the CID is generated correctly
+        // For a real test, you might compare the cid with a known value
+        assert_eq!(cid.version(), cid::Version::V1);
+        assert_eq!(cid.codec(), multicodec::Codec::DagCbor.code());
+
+        // For demonstration: hash the data using the same logic to get the expected hash
+        let expected_mh = multihash_codetable::Code::Sha2_256.digest(data);
+
+        // Compare multihashes
+        assert_eq!(cid.hash(), &expected_mh);
+    }
+}
