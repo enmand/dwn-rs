e9f3f08f062b7f1ba0b970e7e519877b4d6a3e46
Dan Enman <enmand@gmail.com>
2024-11-19 19:39:39 -0400

    feat: add JWS signature calculation for Write fields

--- crates/dwn-rs-core/src/auth/mod.rs
+++ crates/dwn-rs-core/src/auth/mod.rs
@@ -1,5 +1,6 @@
 pub mod authorization;
+pub mod encryption;
 pub mod jws;
 
 pub use authorization::{Authorization, AuthorizationDelegatedGrant, AuthorizationOwner};
-pub use jws::JWS;
+pub use jws::{JwsError, JWS}; // TODO: JWS -> Jws
